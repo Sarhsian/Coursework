628db0285ebe6fa9e0ed23e6477083733fbfe829
sargsyan.mikhail.2017 <sargsyan.mikhail.2017@gmail.com>
2018-10-09 19:38:50 +0300

    Add project files.

---
+++ ComputerTechnic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coursework
+{
+    abstract class ComputerTechnic
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Description { get; set; }
+        public decimal PriceUAH { get; set; }
+        public decimal PriceUSD { get; set; }
+        public int AmountInStock { get; set; }
+    }
+}
